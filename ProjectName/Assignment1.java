9acff20cbb1b7759c88fa7711b912e6eb5b096cd
xxpandir <xxpandir@W5CG0270ZX4>
2022-08-07 00:55:23 +0530

    Pushing the code for the first time

---
+++ ProjectName/Assignment1.java
@@ -0,0 +1,10 @@
+package DeepakPackage;
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ ProjectName/Assignment2.java
@@ -0,0 +1,10 @@
+package DeepakPackage;
+
+public class Assignment2 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
